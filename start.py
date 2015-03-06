5be4af8d5859ab73dcce9632b83cb673c3c4c900
Nelson Lee <mushy1693@live.com>
2015-03-05 17:23:51 -0800

    add start.py

---
+++ start.py
@@ -0,0 +1,38 @@
+import pyglet #to play sound
+import sys, png, random #png used for the initial image
+from Tkinter import * #all for convenience 
+from PIL import Image, ImageTk, ImageOps 
+#Image is Pillow, ImageTK is the way to use images within tkinter, ImageOps used to work with the image after resizing
+
+
+def main():
+   root = Tk()
+   can = Canvas(root, width=400, height=400, borderwidth=5, background='white').pack()
+   
+   def thatOne(op, ran):
+      song = pyglet.media.load('dog_growl3.wav')
+      song.play()
+      pyglet.app.run()
+
+      if op == ran :
+              print "correct"
+      else :
+              print "incorrect"
+              
+      print "{}{}{}{}".format("You chose ", op," correct was: ", ran)
+      next = Button(root, text="Next Question", command=lambda: game()).pack(side=LEFT)
+   
+   def game():  
+      ran = random.randrange(1,5)   
+      #if ran = prev:
+              #ran = random.randrange(1,5)
+      #else:                  
+      b1 = Button(root, text="Option1", command=lambda: thatOne(1, ran)).pack(side=LEFT)
+      b2 = Button(root, text="Option2", command=lambda: thatOne(2, ran)).pack(side=LEFT)
+      b3 = Button(root, text="Option3", command=lambda: thatOne(3, ran)).pack(side=LEFT)
+      b4 = Button(root, text="Option4", command=lambda: thatOne(4, ran)).pack(side=LEFT)
+   
+   game()
+   root.mainloop()  
+
+main()
