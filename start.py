0df65fded61403b7536620360b1ab886c4968018
Michael <aguana321@yahoo.com>
2015-03-10 17:27:04 -0800

    organized player to a function

--- start.py
+++ start.py
@@ -1,33 +1,33 @@
 import pyglet #to play sound
-import sys, random#, png #png used for the initial image
+import sys, random, png #png used for the initial image
 from Tkinter import * #all for convenience 
 from PIL import Image, ImageTk, ImageOps 
 #Image is Pillow, ImageTK is the way to use images within tkinter, ImageOps used to work with the image after resizing
-
-#player = pyglet.media.Player()
-#correct = pyglet.media.load('correct.wav', streaming=False)
-#incorrect = pyglet.media.load('wrong.wav', streaming=False)
-#player.queue(correct)
-#player.queue(incorrect)
 
 
 def main():
    root = Tk()
    can = Canvas(root, width=400, height=400, borderwidth=5, background='white').pack()
-   
+
+   def buttonSounds():
+      global player
+      player = pyglet.media.Player()
+      correct = pyglet.media.load('correct.wav', streaming=False)
+      incorrect = pyglet.media.load('wrong.wav', streaming=False)
+      player.queue(correct)
+      player.queue(incorrect)
+      
    def thatOne(op, ran):
 
       if op == ran :
               print "correct"
-              player = pyglet.media.Player()
-              correct = pyglet.media.load('correct.wav', streaming=False)
-              player.queue(correct)
+              buttonSounds()
               player.play()
+              
       else :
               print "incorrect"
-              player = pyglet.media.Player()
-              false = pyglet.media.load('wrong.wav', streaming=False)
-              player.queue(false)
+              buttonSounds()
+              player.next()
               player.play()
               
       print "{}{}{}{}".format("You chose ", op," correct was: ", ran)
