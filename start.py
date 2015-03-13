b40bcce2b8201c8c19c1d9f43247d7c93e103c6c
Nelson Lee <mushy1693@live.com>
2015-03-12 17:01:37 -0700

    button refactor

--- start.py
+++ start.py
@@ -14,17 +14,30 @@ from PIL import Image, ImageTk, ImageOps
 def main():
    root = Tk()
    can = Canvas(root, width=400, height=400, borderwidth=5, background='white').pack()
-   
+
+   def check(getCheck):
+
+      if(getCheck == "correct"):
+        correctLabel = 'correct'
+      else:
+        correctLabel = 'incorrect'
+      
+      toplevel = Toplevel()
+      label1 = Label(toplevel, text=correctLabel, height=10, width=30)
+      label1.pack()
+     
    def thatOne(op, ran):
 
       if op == ran :
               print "correct"
+              check("correct")
               player = pyglet.media.Player()
               correct = pyglet.media.load('correct.wav', streaming=False)
               player.queue(correct)
               player.play()
       else :
               print "incorrect"
+              check("incorrect")
               player = pyglet.media.Player()
               false = pyglet.media.load('wrong.wav', streaming=False)
               player.queue(false)
@@ -33,15 +46,17 @@ def main():
       print "{}{}{}{}".format("You chose ", op," correct was: ", ran)
       next = Button(root, text="Next Question", command=lambda: game()).pack(side=LEFT)
    
-   def game():  
-      ran = random.randrange(1,5)   
-      #if ran = prev:
-              #ran = random.randrange(1,5)
-      #else:                  
-      b1 = Button(root, text="Option1", command=lambda: thatOne(1, ran)).pack(side=LEFT)
-      b2 = Button(root, text="Option2", command=lambda: thatOne(2, ran)).pack(side=LEFT)
-      b3 = Button(root, text="Option3", command=lambda: thatOne(3, ran)).pack(side=LEFT)
-      b4 = Button(root, text="Option4", command=lambda: thatOne(4, ran)).pack(side=LEFT)
+   def game(): 
+
+      ran = random.randrange(1,5) 
+
+      def createButton(response , option):
+        return Button(root, text=response, command=lambda: thatOne(option, ran)).pack(side=LEFT)
+               
+      b1 = createButton("Option1" , 1)
+      b2 = createButton("Option2" , 2)
+      b3 = createButton("Option3" , 3)
+      b4 = createButton("Option4" , 4)
    
    game()
    root.mainloop()  
