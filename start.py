f9ce10112d827f19d6cfaea5e6fc64f50dd0cfc3
Tyler Nicholson <tnicholson@tylers-mbp.attlocal.net>
2015-03-19 00:56:41 -0700

    we now have an endgame and a quit button, need to add os to imports, no install needed

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import pyglet #to play sound
-import sys, random, png #png used for the initial image
+import sys, os, random, png #png used for the initial image
 from Tkinter import * #all for convenience 
 from PIL import Image, ImageTk, ImageOps 
 #Image is Pillow, ImageTK is the way to use images within tkinter, ImageOps used to work with the image after resizing
@@ -48,7 +48,8 @@ def main():
       label1 = Label(toplevel, text=correctLabel, height=10, width=30, background = "tan4")
       label1.pack()
    
-   # Creates a random array to hold music test values 
+   # Creates a random array to hold music test values
+   #http://stackoverflow.com/questions/21744963/how-to-generate-a-range-of-random-numbers-in-python-without-repetition 
    def createRandomArray():
       for x in range (1, 5):
         ran = random.randint(1, 4)
@@ -127,13 +128,15 @@ def main():
    # Create play button
    play = Button(bottomFrame, text="Play", command=lambda: game(), font=("Helvetica", 18))
    play.pack(side=TOP)
+   quit = Button(bottomFrame, text="Quit", command=lambda: exit(), font=("Helvetica", 18))
+   quit.pack(side=BOTTOM)
 
    # Define game feature function
    def game():
-
       # Destroy the title and play button when game start
       removeWidget(title)
       removeWidget(play)
+      removeWidget(quit)
 
       # Set the size and color of the root window
       root.geometry("%dx%d%+d%+d" % (900, 400, 0, 0))
@@ -141,14 +144,17 @@ def main():
 
       # destroy everything on the frame and move on to next game
       def nextGame():
-
-        removeWidget(scoreLabel)
-        removeWidget(coverButton1)
-        removeWidget(coverButton2)
-        removeWidget(coverButton3)
-        removeWidget(coverButton4)
-        removeWidget(nextButton)
-        game()
+        global count
+        if count == 4:
+           endgame()
+        else:
+           removeWidget(scoreLabel)
+           removeWidget(coverButton1)
+           removeWidget(coverButton2)
+           removeWidget(coverButton3)
+           removeWidget(coverButton4)
+           removeWidget(nextButton)
+           game()
 
       # Creates label for displaying scores
       scoreLabel = Label(bottomFrame, text= "Score: " + str(score), font=("Helvetica", 25), background = "peachpuff4")
@@ -171,6 +177,14 @@ def main():
       # Create the next question button
       nextButton = Button(topFrame, text="Next Question", command=lambda: nextGame(), font=("Helvetica", 16))
       nextButton.pack(side=LEFT)
+     
+   def endgame():
+      #https://www.daniweb.com/software-development/python/code/260268/restart-your-python-program-
+      python = sys.executable
+      os.execl(python, python, * sys.argv)
+      
+   def exit():
+      sys.exit()
 
    #create random array
    createRandomArray()
